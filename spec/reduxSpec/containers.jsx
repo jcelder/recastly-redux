415df9242fd6dad7bb9b40b52af92ac5b765d93e
jcelder <jcelder88@gmail.com>
2018-10-03 17:15:11 -0700

    Complete basic requirements

--- spec/reduxSpec/containers.jsx
+++ spec/reduxSpec/containers.jsx
@@ -27,6 +27,7 @@ describe('containers', function() {
       store.dispatch.reset();
     });
     it('should have a prop called handleSearchInputChange which dispatches handleVideoSearch', function() {
+      debugger;
       component.props().handleSearchInputChange('kitten mittens');
       expect(component.props().handleSearchInputChange).to.be.a('function');
       expect(store.dispatch.callCount).to.equal(1);
